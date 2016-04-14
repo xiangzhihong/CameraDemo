54d6e75ca02875f2b8eef92adf6a23ae4ac12b30
xiang zhihong <xiangzhihong8@sina.com>
2016-04-14 19:29:31 +0800

    Camera

---
+++ app/src/main/java/xzh/com/camerademo/MainActivity.java
@@ -0,0 +1,13 @@
+package xzh.com.camerademo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
